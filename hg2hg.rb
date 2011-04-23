9d7f1ee3e81615605f116dd8b7ff4ec86a97fcf1
Tim Kellogg <tkellogg@kpaonline.com>
2011-04-23 14:43:57 -0600

    have a working commit structure
    revs are now ordered with 0 being first commit in time. Multiple parents are also recognized

--- hg2hg.rb
+++ hg2hg.rb
@@ -32,40 +32,41 @@ class Repo
 	def initialize(path)
 		@path = path
 		init_revs
+		finalize_revs
 	end
 	
 	def init_revs
 		Dir.chdir @path
 		# this would normally be a terrible format to read, but it's not a bad
 		# format for parsing strings
-		text = `hg log --template "::::num {rev}\\n::::hash {node}\\n::::parents {parents}\\n::::author {author}\\n::::msg {desc}\\n::::date {date}\\n::::branch {branches}\\n::::end\\n"`
-		@revs = []
+		text = `hg log -l 9999999999 --template "::::num {rev}\\n::::hash {node}\\n::::parents {parents}\\n::::author {author}\\n::::msg {desc}\\n::::date {date|isodatesec}\\n::::branch {branches}\\n::::end\\n"`
+		@revs = Array.new
 		rev = Rev.new()
 		text.each_line { |ln|
 			if ln.start_with? '::::end'
 				cur = nil
-				@revs.push rev
+				@revs.unshift rev
 				rev = Rev.new
 			elsif ln.start_with? '::::num'
-				rev.num << ln[8..-1]
+				rev.num << ln[8..-1].strip
 				cur = rev.num
 			elsif ln.start_with? '::::hash'
-				rev.hash << ln[9..-1]
+				rev.hash << ln[9..-1].strip
 				cur = rev.num
 			elsif ln.start_with? '::::parents'
-				rev.parents << ln[9..-1]
+				rev.parents << ln[12..-1].strip
 				cur = rev.num
 			elsif ln.start_with? '::::author'
-				rev.author << ln[9..-1]
+				rev.author << ln[11..-1].strip
 				cur = rev.num
 			elsif ln.start_with? '::::msg'
-				rev.msg << ln[9..-1]
+				rev.msg << ln[8..-1].strip
 				cur = rev.num
 			elsif ln.start_with? '::::date'
-				rev.date << ln[9..-1]
+				rev.date << ln[9..-1].strip
 				cur = rev.num
 			elsif ln.start_with? '::::branch'
-				rev.branch << ln[9..-1]
+				rev.branch << ln[11..-1].strip
 				cur = rev.num
 			elsif cur != nil
 				cur << ln
@@ -75,8 +76,34 @@ class Repo
 		@revs.push rev
 	end
 	
+	def finalize_revs
+		last = @revs.first
+		@revs[1..-1].each { |r|
+			if !r.parents
+				# if the {parents} field was blank, the last rev was
+				# the parent
+				r.parents = last
+			else
+				revs = Array.new
+				r.parents.scan(/(\d+):([a-f0-9]+)/) { |m|
+					hash = m[1]
+					rev_list = @revs.select { |x| x.hash.start_with? hash }
+					if rev_list.length > 0
+						revs << rev_list.first
+					end
+				}
+				r.parents = revs
+			end
+			last = r
+		}
+	end
+	
 	def path
 		@path
+	end
+	
+	def revs
+		@revs
 	end
 end
 
@@ -97,7 +124,22 @@ class Rev
 	def msg() @msg end
 	def date() @date end
 	def branch() @branch end
+	
+	def parents= (parent)
+		@parents = parent
+	end
+	
+	def to_s
+		"Rev #{@num} (#{@hash})
+parents: #{@parents}
+author: #{@author}
+comment: #{@msg}
+date: #{@date}
+branch: #{@branch}"
+	end
 end
 
 check_params
-Dir.chdir $from.path
+$into.revs.each do |rev|
+	puts rev.num
+end
